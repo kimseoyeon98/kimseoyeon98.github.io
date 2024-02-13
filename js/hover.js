dbacfb758e76646a16762239b56128cdf777226c
cat7570 <cat7570@naver.com>
2024-02-13 16:11:41 +0900

    호버 효과

---
+++ js/hover.js
@@ -0,0 +1,9 @@
+// my coreer
+
+let careerOnclick01 = document.querySelector('.careerOnclick01');
+
+careerOnclick01.addEventListener('click', function() {
+    careerOnclick01.classList.toggle('displayBlock');
+  });
+
+  // 오류 : 자기 자신한테 붙으면 안되고, ul에 붙어야함.
